3337cb20562fcfc005108df6e70829b90277bad1
Andrey Minin <ewkjf@mail.ru>
2021-09-05 14:03:19 +0300

    refactored code to make it possible to work with queues

--- sequential/main.py
+++ sequential/main.py
@@ -1,7 +1,5 @@
-import queue
-from functools import cmp_to_key
-
 from typing import List
+from queue import Queue
 
 
 class Item:
@@ -39,31 +37,28 @@ class Node:
 
     def __init__(self, le: int, p: int, b: int, w: float):
         self.level = le
-        self.weight = w
         self.profit = p
         self.bound = b
+        self.weight = w
 
 
 class Solver:
 
     def __init__(self):
         self.max_profit = 0
+        self.tasks_q = Queue()
 
     def initialize_amount(self):
         return 5
 
-
     def initialize_weight(self):
         return 10
-
 
     def initialize_items(self):
         return [Item(2, 40), Item(3.1, 50), Item(1.98, 100), Item(5, 95), Item(3, 30)]
 
-
     def cmp(self, i1: Item, i2: Item):
         return i1.value / i1.weight > i2.value / i2.weight
-
 
     def bound(self, u: Node, n: int, w: int, arr: List[Item]) -> int:
         if u.weight > w:
@@ -82,36 +77,29 @@ class Solver:
 
         return profit_bound
 
-
-
     def ramify(self, u: Node, n: int, arr: List[Item], W: int) -> None:
-        v = Node(0, 0, 0, 0)
+        v_in = Node(0, 0, 0, 0)
         if u.level == n - 1:
             return
 
-        v.level = u.level + 1
-        v.weight = u.weight + arr[v.level].weight
-        v.profit = u.profit + arr[v.level].value
+        v_in.level = u.level + 1
+        v_in.weight = u.weight + arr[v_in.level].weight
+        v_in.profit = u.profit + arr[v_in.level].value
 
-        if v.weight <= W and v.profit > self.max_profit:
-            self.max_profit = v.profit
+        if v_in.weight <= W and v_in.profit > self.max_profit:
+            self.max_profit = v_in.profit
 
-        v.bound = self.bound(v, n, W, arr)
-        max_profit1 = 0
-        if v.bound > self.max_profit:
-            self.ramify(v, n, arr, W)
-            # max_profit1 = ramify(v, n, arr, max_profit, W)
+        v_in.bound = self.bound(v_in, n, W, arr)
+        if v_in.bound > self.max_profit:
+            self.tasks_q.put(v_in)
 
-        v.weight = u.weight
-        v.profit = u.profit
-        v.bound = self.bound(v, n, W, arr)
-        max_profit2 = 0
-        if v.bound > self.max_profit:
-            self.ramify(v, n, arr, W)
-            # max_profit2 = ramify(v, n, arr, max_profit, W)
-        #
-        # max_profit = max(max_profit1, max_profit2, max_profit)
-        # return max_profit
+        v_out = Node(0, 0, 0, 0)
+        v_out.level = u.level + 1
+        v_out.weight = u.weight
+        v_out.profit = u.profit
+        v_out.bound = self.bound(v_out, n, W, arr)
+        if v_out.bound > self.max_profit:
+            self.tasks_q.put(v_out)
 
 
 if __name__ == '__main__':
@@ -122,9 +110,13 @@ if __name__ == '__main__':
     items.sort()
     items.reverse()
 
-    root = Node(-1, 0, 0, 0)
+    # root = Node(-1, 0, 0, 0)
+    root = Node(0, items[0].value, 0, items[0].weight)
+    root.bound = solver.bound(root, n, W, items)
 
     print(items)
-    solver.ramify(root, n, items, W)
+    solver.tasks_q.put(root)
+    while not solver.tasks_q.empty():
+        solver.ramify(solver.tasks_q.get(), n, items, W)
     print(solver.max_profit)
 
